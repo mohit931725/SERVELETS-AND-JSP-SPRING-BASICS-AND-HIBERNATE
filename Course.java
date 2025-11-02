512bfd01ccf9e302ea69cc13c83677de596d0e94
mohit931725 <141955125+mohit931725@users.noreply.github.com>
2025-11-02 15:21:46 +0530

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,13 @@
+package com.nimbus.springdi;
+
+public class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
